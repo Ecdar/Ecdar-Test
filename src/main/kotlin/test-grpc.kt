e6864d0859c0e120680c4779c426020c1bf60f95
Sebastian Lund <sebastianlund112@gmail.com>
2021-10-20 11:11:44 +0200

    Initial setup of grpc protos

---
+++ src/main/kotlin/test-grpc.kt
@@ -0,0 +1,24 @@
+import EcdarProtoBuf.ComponentProtos
+import EcdarProtoBuf.EcdarBackendGrpc
+import EcdarProtoBuf.QueryProtos
+import io.grpc.ManagedChannelBuilder
+import java.io.File
+
+fun main() {
+    val channel = ManagedChannelBuilder.forTarget("127.0.0.1:50051") // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+        // needing certificates.
+        .usePlaintext()
+        .build()
+
+
+    val stub = EcdarBackendGrpc.newBlockingStub(channel)
+
+    val component = ComponentProtos.Component.newBuilder().setJson(File("samples/json/AG/Components/A.json").readText()).build()
+    val components = QueryProtos.ComponentsUpdateRequest.newBuilder().addComponents(component).build()
+    stub.updateComponents(components)
+
+    val query = QueryProtos.Query.newBuilder().setId(0).setQuery("refinement: A<=A").build()
+
+    val response = stub.sendQuery(query)
+    println("Refinement is ${response.refinement.success}")
+}
