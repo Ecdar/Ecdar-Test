d3f42bf2cb6763f332a18f11c454385c81cd7af1
Sebastian Lund <sebastianlund112@gmail.com>
2022-01-24 17:46:34 +0100

    Cleanup after running junit tests

--- src/test/kotlin/GeneratedTests.kt
+++ src/test/kotlin/GeneratedTests.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.function.Executable
 import parsing.EngineConfiguration
 import parsing.parseEngineConfigurations
@@ -11,12 +12,27 @@ import proofs.addAllProofs
 import tests.Test
 import tests.testgeneration.addAllTests
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GeneratedTests {
+
+    val engines = parseEngineConfigurations()
+
+    @AfterAll
+    fun cleanup() {
+        println("Cleaning up")
+        engines.forEach { it.terminate() }
+    }
+
+
+    @org.junit.jupiter.api.Test
+    fun test() {
+        assertTrue(true)
+    }
 
     @TestFactory
     fun main(): Iterable<DynamicTest?> {
         val tests = generateTests()
-        val engines = parseEngineConfigurations()
+
 
         return createExecutableJUnitTests(engines, tests)
     }
@@ -44,9 +60,16 @@ class GeneratedTests {
         return dynamicTests
     }
 
-    private fun createJUnitTest(executor: Executor, test: Test): DynamicTest {
+    private fun createJUnitTest(executor: Executor, test: tests.Test): DynamicTest {
         val testName = "${executor.engineConfig.name}::${test.testSuite}::${test.projectPath}::${test.queries().joinToString("; ")}"
-        val testBody = Executable { assertTrue(executor.runTest(test).result, "Test failed: $testName") }
+        val testBody = Executable {
+            println("Testing $testName")
+            assertTrue(executor.runTest(test).result!!, "Test failed: $testName") }
         return dynamicTest(testName, testBody)
+    }
+
+    protected fun finalize() {
+        println("Forcefully terminating all processes")
+        engines.forEach { it.terminate() }
     }
 }
