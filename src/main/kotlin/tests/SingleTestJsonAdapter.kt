a70b94888ba16008d5a026bef42d3312c66addf1
aavild <anders@aavild.dk>
2023-10-21 21:43:10 +0200

    Fixed Results.kt and Plotting.kt

---
+++ src/main/kotlin/tests/SingleTestJsonAdapter.kt
@@ -0,0 +1,12 @@
+package tests
+
+import com.beust.klaxon.TypeAdapter
+import kotlin.reflect.KClass
+
+class SingleTestJsonAdapter: TypeAdapter<SingleTest> {
+    override fun classFor(type: Any): KClass<out SingleTest> = when(type as String) {
+        "NotSatisfiedTest" -> NotSatisfiedTest::class
+        "SatisfiedTest" -> SatisfiedTest::class
+        else -> throw IllegalArgumentException("Unknown type: $type")
+    }
+}
